381bad407d1a909f63e4b70954017646c2791808
lorra <lorra@DESKTOP-EVSV1E0>
2022-10-11 18:17:26 -0300

    Adicionar arquivos de projeto.

---
+++ ChapterBET6/Contexts/sqlcontext.cs
@@ -0,0 +1,28 @@
+﻿using ChapterBET6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChapterBET6.Contexts
+{
+    public class sqlcontext: DbContext
+    {
+        public sqlcontext()
+        {
+        }
+        public sqlcontext(DbContextOptions<sqlcontext>options): base(options)
+        {
+        }
+        // vamos utilizar esse método para configurar o banco de dados
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+                //optionsBuilder.UseSqlServer("DataSource = DESKTOP-EVSV1E0\\SQLEXPRESS; initial catalog = Chapter;User Id = sa; Password = 190217");
+                optionsBuilder.UseSqlServer("Server = DESKTOP-EVSV1E0\\SQLEXPRESS; Database = Chapter;User Id = sa; Password = 190217;");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+
+    }
+}
---
+++ ChapterBET6/Repositories/LivroRepository.cs
@@ -0,0 +1,24 @@
+﻿using ChapterBET6.Contexts;
+using ChapterBET6.Models;
+
+namespace ChapterBET6.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly sqlcontext _context;
+        public LivroRepository(sqlcontext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+
+
+
+        
+
+    }
+}
