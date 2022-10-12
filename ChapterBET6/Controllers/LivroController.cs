9770a6fa3d2efd59234e00b5ff1b1d4a265d965e
lorra <lorra@DESKTOP-EVSV1E0>
2022-10-12 13:33:38 -0300

    push

--- ChapterBET6/Controllers/LivroController.cs
+++ ChapterBET6/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using ChapterBET6.Repositories;
+﻿using ChapterBET6.Models;
+using ChapterBET6.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,6 +25,77 @@ namespace ChapterBET6.Controllers
             {
                 return Ok(_livroRepository.Listar());
 
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro livroBuscado = _livroRepository.BuscarPorId(id);
+
+              if (livroBuscado == null)
+              {
+                    return NotFound();
+
+              }
+
+                return Ok(livroBuscado);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro nomeL)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(nomeL);
+
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+
+                return Ok("livro removido com sucesso");
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Alterar(int id, Livro nomel)
+        {
+            try
+            {
+                _livroRepository.Alterar(id, nomel);
+
+                return StatusCode(204);
             }
             catch (Exception e)
             {
--- ChapterBET6/Repositories/LivroRepository.cs
+++ ChapterBET6/Repositories/LivroRepository.cs
@@ -6,6 +6,9 @@ namespace ChapterBET6.Repositories
     public class LivroRepository
     {
         private readonly sqlcontext _context;
+
+        public object?[]? Id { get; private set; }
+
         public LivroRepository(sqlcontext context)
         {
             _context = context;
@@ -16,9 +19,41 @@ namespace ChapterBET6.Repositories
             return _context.Livros.ToList();
         }
 
+        public Livro BuscarPorId(int id)
+        {
+            return _context.Livros.Find(id);
+        }
 
+        public void Cadastrar(Livro nomeL)
+        {
+            _context.Livros.Add(nomeL);
 
-        
+            _context.SaveChanges();
+        }
 
+        public void Deletar(int id)
+        {
+            Livro nomeL = _context.Livros.Find(id);
+
+            _context.Livros.Remove(nomeL);
+
+            _context.SaveChanges();
+        }
+
+        public void Alterar(int id, Livro nomeL)
+        {
+            Livro livroBuscado = _context.Livros.Find(id);
+            if (livroBuscado != null)
+            {
+                livroBuscado.Titulo = nomeL.Titulo;
+                livroBuscado.QuantidadePaginas = nomeL.QuantidadePaginas;
+                livroBuscado.Disponivel = nomeL.Disponivel;
+                _context.Livros.Update(livroBuscado);
+
+                _context.SaveChanges();
+               
+            }
+
+        }
     }
 }
